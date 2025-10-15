9a4fdac89e45897fb9a43b093ba0e55a8e1acfff
RedWolf77 <romario270704@mail.ru>
2025-10-15 17:56:33 +0200

    index.php code added

--- lab3/project/nginx-lab/www/index.php
+++ lab3/project/nginx-lab/www/index.php
@@ -1,3 +1,17 @@
-<?php
-phpinfo();
-?>
+<?php session_start(); ?>
+
+<?php if(isset($_SESSION['username'])): ?>
+    <p>Данные из сессии:</p>
+    <ul>
+        <li>Имя: <?= $_SESSION['name'] ?></li>
+        <li>Количество порций: <?= $_SESSION['quantity'] ?></li>
+        <li>Блюдо: <?= $_SESSION['dish'] ?></li>
+        <li>Соус: <?= $_SESSION['sauce'] ?></li>
+        <li>Тип доставки: <?= $_SESSION['delivery'] ?></li>
+    </ul>
+<?php else: ?>
+    <p>Данных пока нет.</p>
+<?php endif; ?>
+
+<a href="form.html">Заполнить форму</a> |
+<a href="view.php">Посмотреть все данные</a>
