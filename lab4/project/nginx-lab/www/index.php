b5ae081ab75681f3b6ebb4db5c7801e1559e595f
RedWolf77 <romario270704@mail.ru>
2025-10-23 16:59:06 +0200

    index.php updated

--- lab4/project/nginx-lab/www/index.php
+++ lab4/project/nginx-lab/www/index.php
@@ -9,6 +9,12 @@
     <?php unset($_SESSION['errors']); ?>
 <?php endif; ?>
 
+<h3>Информация о пользователе:</h3>
+<?php foreach ($info as $key => $val): ?>
+    <?= htmlspecialchars($key) ?>: <?= htmlspecialchars($val) ?><br>
+<?php endforeach; ?>
+<hr>
+
 <?php if(isset($_SESSION['name'])): ?>
     <p>Данные из сессии:</p>
     <ul>
@@ -18,7 +24,7 @@
         <li>Соус: <?= $_SESSION['sauce'] ?></li>
         <li>Тип доставки: <?= $_SESSION['delivery'] ?></li>
     </ul>
-    
+
     <?php if(isset($_SESSION['api_data'])): ?>
         <h3>Данные из API:</h3>
         <pre><?php print_r($_SESSION['api_data']); ?></pre>
