aefba5adda579380a44558507784022e01b3df7c
RedWolf77 <romario270704@mail.ru>
2025-10-29 21:10:54 +0200

    lab5 added

---
+++ lab5/project/nginx-lab/www/index.php
@@ -0,0 +1,43 @@
+<?php session_start(); ?>
+
+<?php
+require_once 'UserInfo.php';
+$info = UserInfo::getInfo();
+?>
+
+<?php if(isset($_SESSION['errors'])): ?>
+    <ul style="color:red;">
+        <?php foreach($_SESSION['errors'] as $error): ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <?php unset($_SESSION['errors']); ?>
+<?php endif; ?>
+
+<h3>Информация о пользователе:</h3>
+<?php foreach ($info as $key => $val): ?>
+    <?= htmlspecialchars($key) ?>: <?= htmlspecialchars($val) ?><br>
+<?php endforeach; ?>
+<hr>
+
+<?php if(isset($_SESSION['name'])): ?>
+    <p>Данные из сессии:</p>
+    <ul>
+        <li>Имя: <?= $_SESSION['name'] ?></li>
+        <li>Количество порций: <?= $_SESSION['quantity'] ?></li>
+        <li>Блюдо: <?= $_SESSION['dish'] ?></li>
+        <li>Соус: <?= $_SESSION['sauce'] ?></li>
+        <li>Тип доставки: <?= $_SESSION['delivery'] ?></li>
+    </ul>
+
+    <?php if(isset($_SESSION['api_data'])): ?>
+        <h3>Данные из API:</h3>
+        <pre><?php print_r($_SESSION['api_data']); ?></pre>
+        <?php unset($_SESSION['api_data']); ?>
+    <?php endif; ?>
+<?php else: ?>
+    <p>Данных пока нет.</p>
+<?php endif; ?>
+
+<a href="form.html">Заполнить форму</a> |
+<a href="view.php">Посмотреть все данные</a>
---
+++ lab5/project/nginx-lab/www/process.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+$name = htmlspecialchars($_POST['name']);
+$quantity = htmlspecialchars($_POST['quantity']);
+$dish = htmlspecialchars($_POST['dish']);
+
+if (isset($_POST['sauce']) && $_POST['sauce'] === 'yes') {
+    $sauce = "с соусом";
+} else {
+    $sauce = "без соуса";
+}
+
+$delivery = isset($_POST['delivery']) ? $_POST['delivery'] : '';
+
+$_SESSION['name'] = $name;
+$_SESSION['quantity'] = $quantity;
+$_SESSION['dish'] = $dish;
+$_SESSION['sauce'] = $sauce;
+$_SESSION['delivery'] = $delivery;
+
+$errors = [];
+if(empty($name)) $errors[] = "Имя не может быть пустым";
+if(empty($dish)) $errors[] = "Блюдо не может быть пустым";
+if ($quantity < 1) $errors[] = "Количество порций должно быть больше одной.";
+
+if(!empty($errors)){
+    $_SESSION['errors'] = $errors;
+    header("Location: index.php");
+    exit();
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once 'ApiClient.php';
+$api = new ApiClient();
+
+$url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+$apiData = $api->request($url);
+
+$_SESSION['api_data'] = $apiData;
+
+$line = $name . ";" . $quantity . ";" . $dish . ";" . $sauce . ";" . $delivery . "\n";
+file_put_contents("data.txt", $line, FILE_APPEND);
+
+setcookie("last_submission", date('Y-m-d H:i:s'), time() + 3600, "/");
+
+header("Location: index.php");
+exit();
