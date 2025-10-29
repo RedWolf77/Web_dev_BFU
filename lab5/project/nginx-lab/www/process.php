de5824e260b792a2eff02ddd2fdd38a194e4a54a
RedWolf77 <romario270704@mail.ru>
2025-10-29 21:33:13 +0200

    process.php updated

--- lab5/project/nginx-lab/www/process.php
+++ lab5/project/nginx-lab/www/process.php
@@ -1,48 +1,16 @@
 <?php
-session_start();
+require 'db.php';
+require 'Order.php';
+
+$order = new Order($pdo);
 
 $name = htmlspecialchars($_POST['name']);
-$quantity = htmlspecialchars($_POST['quantity']);
 $dish = htmlspecialchars($_POST['dish']);
+$quantity = intval($_POST['quantity']);
+$sauce = isset($_POST['sauce']) ? 1 : 0;
+$delivery_type = htmlspecialchars($_POST['delivery']);
 
-if (isset($_POST['sauce']) && $_POST['sauce'] === 'yes') {
-    $sauce = "с соусом";
-} else {
-    $sauce = "без соуса";
-}
-
-$delivery = isset($_POST['delivery']) ? $_POST['delivery'] : '';
-
-$_SESSION['name'] = $name;
-$_SESSION['quantity'] = $quantity;
-$_SESSION['dish'] = $dish;
-$_SESSION['sauce'] = $sauce;
-$_SESSION['delivery'] = $delivery;
-
-$errors = [];
-if(empty($name)) $errors[] = "Имя не может быть пустым";
-if(empty($dish)) $errors[] = "Блюдо не может быть пустым";
-if ($quantity < 1) $errors[] = "Количество порций должно быть больше одной.";
-
-if(!empty($errors)){
-    $_SESSION['errors'] = $errors;
-    header("Location: index.php");
-    exit();
-}
-
-require_once __DIR__ . '/vendor/autoload.php';
-require_once 'ApiClient.php';
-$api = new ApiClient();
-
-$url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
-$apiData = $api->request($url);
-
-$_SESSION['api_data'] = $apiData;
-
-$line = $name . ";" . $quantity . ";" . $dish . ";" . $sauce . ";" . $delivery . "\n";
-file_put_contents("data.txt", $line, FILE_APPEND);
-
-setcookie("last_submission", date('Y-m-d H:i:s'), time() + 3600, "/");
+$order_id = $order->add($name, $dish, $quantity, $sauce, $delivery_type);
 
 header("Location: index.php");
 exit();
