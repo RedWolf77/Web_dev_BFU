6752aa40054f1b4ffc4e3230ff78cd8500cd2216
RedWolf77 <romario270704@mail.ru>
2025-10-29 21:35:25 +0200

    index.php updated

--- lab5/project/nginx-lab/www/index.php
+++ lab5/project/nginx-lab/www/index.php
@@ -1,43 +1,21 @@
-<?php session_start(); ?>
-
 <?php
-require_once 'UserInfo.php';
-$info = UserInfo::getInfo();
+require 'db.php';
+require 'Order.php';
+
+$order = new Order($pdo);
+$all = $order->getAll();
 ?>
-
-<?php if(isset($_SESSION['errors'])): ?>
-    <ul style="color:red;">
-        <?php foreach($_SESSION['errors'] as $error): ?>
-            <li><?= $error ?></li>
-        <?php endforeach; ?>
-    </ul>
-    <?php unset($_SESSION['errors']); ?>
-<?php endif; ?>
-
-<h3>Информация о пользователе:</h3>
-<?php foreach ($info as $key => $val): ?>
-    <?= htmlspecialchars($key) ?>: <?= htmlspecialchars($val) ?><br>
+<h2>Все заказы:</h2>
+<ul>
+<?php foreach($all as $row): ?>
+    <li>
+        <?= $row['name'] ?>, 
+        Блюдо: <?= $row['dish'] ?>, 
+        Количество: <?= $row['quantity'] ?>, 
+        Соус: <?= $row['sauce'] ? 'Да' : 'Нет' ?>, 
+        Доставка: <?= $row['delivery_type'] ?>
+    </li>
 <?php endforeach; ?>
-<hr>
+</ul>
 
-<?php if(isset($_SESSION['name'])): ?>
-    <p>Данные из сессии:</p>
-    <ul>
-        <li>Имя: <?= $_SESSION['name'] ?></li>
-        <li>Количество порций: <?= $_SESSION['quantity'] ?></li>
-        <li>Блюдо: <?= $_SESSION['dish'] ?></li>
-        <li>Соус: <?= $_SESSION['sauce'] ?></li>
-        <li>Тип доставки: <?= $_SESSION['delivery'] ?></li>
-    </ul>
-
-    <?php if(isset($_SESSION['api_data'])): ?>
-        <h3>Данные из API:</h3>
-        <pre><?php print_r($_SESSION['api_data']); ?></pre>
-        <?php unset($_SESSION['api_data']); ?>
-    <?php endif; ?>
-<?php else: ?>
-    <p>Данных пока нет.</p>
-<?php endif; ?>
-
-<a href="form.html">Заполнить форму</a> |
-<a href="view.php">Посмотреть все данные</a>
+<a href="form.html">Сделать новый заказ</a>
