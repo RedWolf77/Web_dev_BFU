3c304c885521e60a8f966351882d5739c4dd0322
RedWolf77 <romario270704@mail.ru>
2025-11-12 17:55:29 +0200

    many bags fixed

--- lab6/project/nginx-lab/www/index.php
+++ lab6/project/nginx-lab/www/index.php
@@ -2,11 +2,8 @@
 
 require 'vendor/autoload.php';
 
-use App\RedisExample;
 use App\ElasticExample;
-use App\ClickhouseExample;
 
 // Elasticsearch
 $elastic = new ElasticExample();
-echo $elastic->indexDocument('books', 1, ['title' => '1984', 'author' => 'Orwell']);
-echo $elastic->search('books', ['author' => 'Orwell']);
+
