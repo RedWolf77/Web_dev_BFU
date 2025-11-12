06610af01eba9a62ec98a0aefd1b121c5ff408e7
RedWolf77 <romario270704@mail.ru>
2025-11-12 18:11:35 +0200

    add_sample_products added

--- lab6/project/nginx-lab/www/src/ElasticExample.php
+++ lab6/project/nginx-lab/www/src/ElasticExample.php
@@ -39,6 +39,41 @@ class ElasticExample
         }
     }
 
+    // добавление тестовых товаров
+    private function addSampleProducts()
+    {
+        $products = [
+            [
+                'name' => 'Телефон Nokia 3310',
+                'description' => 'Легендарный сотовый телефон высокой уровни надежности',
+                'price' => 4499.99,
+                'category' => 'electronics'
+            ],
+            [
+                'name' => 'Ноутбук MacBook Air M4',
+                'description' => 'Мощный современный ноутбук с передовым процессором M4',
+                'price' => 99999.99,
+                'category' => 'electronics'
+            ],
+            [
+                'name' => 'Футболка хлопковая',
+                'description' => 'Удобная футболка из натурального хлопка, стильный цвет хаки',
+                'price' => 1999.99,
+                'category' => 'clothing'
+            ],
+            [
+                'name' => 'Книга "Программирование на PHP"',
+                'description' => 'Учебник по программированию на языке PHP для начинающих',
+                'price' => 2499.99,
+                'category' => 'books'
+            ]
+        ];
+
+        foreach ($products as $id => $product) {
+            $this->indexDocument($this->index, $id + 1, $product);
+        }
+    }
+
     public function indexDocument($index, $id, $data)
     {
         $response = $this->client->put("$index/_doc/$id", [
