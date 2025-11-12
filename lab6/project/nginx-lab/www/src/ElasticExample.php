f7199f18c17b00a1ffad7691fedd144933e658e4
RedWolf77 <romario270704@mail.ru>
2025-11-12 18:04:33 +0200

    product index creation added

--- lab6/project/nginx-lab/www/src/ElasticExample.php
+++ lab6/project/nginx-lab/www/src/ElasticExample.php
@@ -7,10 +7,36 @@ use App\Helpers\ClientFactory;
 class ElasticExample
 {
     private $client;
+    private $index = 'products';
 
     public function __construct()
     {
         $this->client = ClientFactory::make('http://localhost:9200/');
+        $this->createIndex();
+    }
+
+    // создание индекса для товаров
+    public function createIndex()
+    {
+        try {
+            $this->client->put($this->index, [
+                'json' => [
+                    'mappings' => [
+                        'properties' => [
+                            'name' => ['type' => 'text'],
+                            'description' => ['type' => 'text'],
+                            'price' => ['type' => 'float'],
+                            'category' => ['type' => 'keyword']
+                        ]
+                    ]
+                ]
+            ]);
+            
+            $this->addSampleProducts();
+            
+        } catch (\Exception $e) {
+            
+        }
     }
 
     public function indexDocument($index, $id, $data)
